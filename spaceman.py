d0a015567dc310c1723146eea5f08d380f78bdd2
Louis Dweck <louisdweck@Louiss-MacBook-Pro.local>
2019-09-11 01:53:40 -0700

    added dictionary

--- spaceman.py
+++ spaceman.py
@@ -9,7 +9,7 @@ def load_word():
     Returns: 
            string: The secret word to be used in the spaceman guessing game
     '''
-    f = open('words.txt', 'r')
+    f = open('/usr/share/dict/words', 'r')
     words_list = f.read().splitlines()
     f.close()
 
@@ -27,11 +27,17 @@ def is_word_guessed(secret_word, letters_guessed):
         secret_word (string): the random word the user is trying to guess.
         letters_guessed (list of strings): list of letters that have been guessed so far.
 
+        'hello'
+        ['f', 'e', 't', 'l']
+
     Returns: 
         bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
     '''
     # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-    pass
+    for letter in secret_word:
+        if letter not in letters_guessed:
+            return False
+    return True
 
 
 def get_guessed_word(secret_word, letters_guessed):
